1a750bf1b298a605223af955b0aaf5df7f1e2d2d
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-09-02 21:38:05 +0700

    Initial Commit

---
+++ iSquareAndSound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iSquareAndSound
+//
+//  Created by Bui Duc Khanh on 9/2/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
